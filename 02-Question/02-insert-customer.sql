e0a3cc32e06fc424b6ba42181129da0491d84ce3
sh1nine96 <98647580+sh1nine96@users.noreply.github.com>
2022-05-23 12:21:02 +0530

    files

---
+++ 02-Question/02-insert-customer.sql
@@ -0,0 +1,9 @@
+delete from customer;
+
+insert into customer values(1, 'AAKASH', 9999999999, 'DELHI', 'M');
+insert into customer values(2, 'AMAN', 9785463215, 'NOIDA', 'M');
+insert into customer values(3, 'NEHA', 9999999999, 'MUMBAI', 'F');
+insert into customer values(4, 'MEGHA', 9994562399, 'KOLKATA', 'F');
+insert into customer values(5, 'PULKIT', 7895999999, 'LUCKNOW', 'M');
+
+select* from customer;
