35323abe4c49edf85eee5e1438d37bdbfa782855
sh1nine96 <98647580+sh1nine96@users.noreply.github.com>
2022-05-23 12:30:06 +0530

    files

---
+++ 02-Question/06-insert-orders.sql
@@ -0,0 +1,20 @@
+delete from orders;
+
+insert into orders values(101, 1500, '2021-10-06', 2, 1);
+insert into orders values(102, 1000, '2021-10-12', 3, 5);
+insert into orders values(103, 30000, '2021-09-16', 5, 2);
+insert into orders values(104, 1500, '2021-10-05', 1, 1);
+insert into orders values(105, 3000, '2021-08-16', 4, 3);
+insert into orders values(106, 1450, '2021-08-18', 1, 9);
+insert into orders values(107, 789, '2021-09-01', 3, 7);
+insert into orders values(108, 780, '2021-09-07', 5, 6);
+insert into orders values(109, 3000, '2021-00-10', 5, 3);
+insert into orders values(110, 2500, '2021-09-10', 2, 4);
+insert into orders values(111, 1000, '2021-09-15', 4, 5);
+insert into orders values(112, 789, '2021-09-16', 4, 7);
+insert into orders values(113, 31000, '2021-09-16', 1, 8);
+insert into orders values(114, 1000, '2021-09-16', 3, 5);
+insert into orders values(115, 3000, '2021-09-16', 5, 3);
+insert into orders values(116, 99, '2021-09-17', 2, 14);
+
+select* from orders;
