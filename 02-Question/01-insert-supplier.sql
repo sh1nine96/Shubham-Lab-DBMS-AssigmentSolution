c898ced7580a268a0b9d224cca6896a325bfc638
sh1nine96 <98647580+sh1nine96@users.noreply.github.com>
2022-05-23 12:18:53 +0530

    files

---
+++ 02-Question/01-insert-supplier.sql
@@ -0,0 +1,9 @@
+delete from supplier;
+
+insert into supplier values(1, 'Rajesh Retails', 'Delhi', 1234567890 );
+insert into supplier values(2, 'Appario Ltd', 'Mumbai', 2589631470 );
+insert into supplier values(3, 'Knome products', 'Banglore', 9785462315 );
+insert into supplier values(4, 'Bansal Retails', 'Kochi', 8975463285 );
+insert into supplier values(5, 'Mittal Ltd.', 'Lucknow', 7898456532 );
+
+select* from supplier;
