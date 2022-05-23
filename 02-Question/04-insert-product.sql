e502a8606a74fd8a963bf925361f81475f584654
sh1nine96 <98647580+sh1nine96@users.noreply.github.com>
2022-05-23 12:25:40 +0530

    files

---
+++ 02-Question/04-insert-product.sql
@@ -0,0 +1,16 @@
+delete from product;
+
+insert into product values(1, 'GTA V', 'Windows 7 and above with i5 processor and 8GB RAM', 2);
+insert into product values(2, 'TSHIRT', 'SIZE-L with Black, Blue and White variations', 5);
+insert into product values(3, 'ROG LAPTOP', 'Windows 10 with 15inch screen, i7 processor, 1TB SSD', 4);
+insert into product values(4, 'OATS', 'Highly Nutritious from Nestle', 3);
+insert into product values(5, 'HARRY POTTER', 'Best Collection of all time by J.K Rowling', 1);
+insert into product values(6, 'MILK', '1L Toned Milk', 3);
+insert into product values(7, 'Boat Earphones', '1.5Meter long Dolby Atmos', 4);
+insert into product values(8, 'Jeans Stretchable', 'Denim Jeans with various sizes and color', 5);
+insert into product values(9, 'Project IGI', 'compatible with windows 7 and above', 2);
+insert into product values(10, 'Hoodie', 'Black GUCCI for 13 yrs and above', 5);
+insert into product values(11, 'Rich Dad Poor Dad', 'Written by Robert Kiyosaki', 1);
+insert into product values(12, 'Train Your Brain', 'By Shireen Stephen', 1);
+
+select* from product;
