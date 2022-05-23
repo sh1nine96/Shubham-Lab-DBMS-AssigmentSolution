ce73a7085185eef2fa155d0f5df33112eaaf50cf
sh1nine96 <98647580+sh1nine96@users.noreply.github.com>
2022-05-23 12:31:24 +0530

    files

---
+++ 02-Question/07-insert-rating.sql
@@ -0,0 +1,20 @@
+delete from rating;
+
+insert into rating values(1, 101, 4);
+insert into rating values(2, 102, 3);
+insert into rating values(3, 103, 1);
+insert into rating values(4, 104, 2);
+insert into rating values(5, 105, 4);
+insert into rating values(6, 106, 3);
+insert into rating values(7, 107, 4);
+insert into rating values(8, 108, 4);
+insert into rating values(9, 109, 3);
+insert into rating values(10, 110, 5);
+insert into rating values(11, 111, 3);
+insert into rating values(12, 112, 4);
+insert into rating values(13, 113, 2);
+insert into rating values(14, 114, 1);
+insert into rating values(15, 115, 1);
+insert into rating values(16, 116, 0);
+
+select* from rating;
