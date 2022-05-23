49f5ea9e5564503b3852d1b6d40eaf7a2683b583
sh1nine96 <98647580+sh1nine96@users.noreply.github.com>
2022-05-23 12:23:32 +0530

    files

---
+++ 02-Question/03-insert-category.sql
@@ -0,0 +1,9 @@
+delete from category;
+
+insert into category values(1, 'BOOKS');
+insert into category values(2, 'GAMES');
+insert into category values(3, 'GROCERIES');
+insert into category values(4, 'ELECTRONICS');
+insert into category values(5, 'CLOTHES');
+
+select* from category;
